417265bd5b32ed309ef3fb06b863ae27aa94ab98
chel-007 <wstones025@gmail.com>
2024-07-29 13:50:30 +0100

    edited

--- app/api/bigquery.ts
+++ app/api/bigquery.ts
@@ -59,11 +59,7 @@ async function queryDailyTransactionVolume() {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-    if (req.method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-    return;
-  }
+  
   console.log('Request received:', req.url, req.query);
 
   const { analysisType }: {
